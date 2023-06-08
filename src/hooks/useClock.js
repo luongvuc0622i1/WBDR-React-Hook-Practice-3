3414cf854c55abefd5cd85f39321ea2a6826bd08
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2023-06-08 18:01:02 +0700

    done

---
+++ src/hooks/useClock.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function useClock() {
+    const [time, setTime] = useState("");
+    const [ampm, setAmpm] = useState("");
+
+    const updateTime = function() {
+        let dateInfo = new Date();
+        let hour = 0;
+        /* Lấy giá trị của giờ */
+        if (dateInfo.getHours() === 0) {
+            hour = 12;
+        } else if (dateInfo.getHours() > 12) {
+            hour = dateInfo.getHours() - 12;
+        } else {
+            hour = dateInfo.getHours();
+        }
+        /* Lấy giá trị của phút */
+        let minutes = dateInfo.getMinutes() < 10 ? parseInt("0" + dateInfo.getMinutes()) : dateInfo.getMinutes();
+        /* Lấy giá trị giây */
+        let seconds = dateInfo.getSeconds() < 10 ? "0" + dateInfo.getSeconds() : dateInfo.getSeconds();
+
+        /* Định dạng ngày */
+        let ampm = dateInfo.getHours() >= 12 ? "PM" : "AM";
+
+        /* Cập nhật state */
+        setAmpm(ampm);
+        setTime(`${hour}:${minutes}:${seconds}`);
+    };
+
+    setInterval(function() {
+        updateTime();
+    }, 1000);
+
+    return [time, ampm]
+}
